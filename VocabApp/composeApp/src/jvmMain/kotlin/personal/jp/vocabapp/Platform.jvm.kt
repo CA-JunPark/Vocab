ba7a61edd1b06c950d195706436421427634d948
CA-JunPark <cskoko5786@gmail.com>
2025-12-25 16:51:17 -0800

    setup Koin

--- VocabApp/composeApp/src/jvmMain/kotlin/personal/jp/vocabapp/Platform.jvm.kt
+++
@@ -1,7 +0,0 @@
-package personal.jp.vocabapp
-
-class JVMPlatform: Platform {
-    override val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-actual fun getPlatform(): Platform = JVMPlatform()
