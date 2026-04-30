ccf6441ae6c598d0d369b655daacf6a26edbad0f
CA-JunPark <cskoko5786@gmail.com>
2026-04-29 22:02:02 -0700

    jvm fix

--- VocabApp/composeApp/src/jvmMain/kotlin/personal/jp/vocabapp/di/platformModule.jvm.kt
+++ VocabApp/composeApp/src/jvmMain/kotlin/personal/jp/vocabapp/di/platformModule.jvm.kt
@@ -6,10 +6,13 @@ import personal.jp.vocabapp.google.LoginHandler
 import personal.jp.vocabapp.google.SecureStorage
 import personal.jp.vocabapp.google.createDataStorage
 import personal.jp.vocabapp.internet.isNetworkAvailable
+import personal.jp.vocabapp.widget.JvmWidgetSyncManager
+import personal.jp.vocabapp.widget.WidgetSyncManager
 
 actual val platformModule = module {
     single<LoginHandler> { JvmLoginHandler() }
     single {createDataStorage(null)}
     single { SecureStorage(get()) }
     single {isNetworkAvailable(null)}
+    single<WidgetSyncManager> { JvmWidgetSyncManager() }
 }
